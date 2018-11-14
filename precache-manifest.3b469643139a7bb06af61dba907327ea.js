08aacb601b704c1058e29090c0e045eb713bd6ac
jintsuu <nipanasovich@gmail.com>
2018-11-14 22:02:16 +0300

    added media-queries

--- precache-manifest.3b469643139a7bb06af61dba907327ea.js
+++ precache-manifest.3b469643139a7bb06af61dba907327ea.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "/static/media/react.f0793d9f.svg"
   },
   {
-    "revision": "9af5a54bf9e70aedb759",
-    "url": "/static/css/main.b5ee61f0.chunk.css"
+    "revision": "269ea8ee5f1aa25c60a6",
+    "url": "/static/css/main.492a570b.chunk.css"
   },
   {
-    "revision": "218b78c2e4c50f0a1c95692276a8e367",
-    "url": "/static/media/160632.218b78c2.jpg"
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/static/media/twitter1.c9f12008.svg"
   },
   {
-    "revision": "b77c17e9e766dc677ffa",
-    "url": "/static/js/1.b77c17e9.chunk.js"
+    "revision": "42ed86b3b3dd970f99f5",
+    "url": "/static/js/1.42ed86b3.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
@@ -44,47 +44,47 @@ self.__precacheManifest = [
     "url": "/static/media/javascript.2d2e31cc.svg"
   },
   {
-    "revision": "9af5a54bf9e70aedb759",
-    "url": "/static/js/main.9af5a54b.chunk.js"
+    "revision": "269ea8ee5f1aa25c60a6",
+    "url": "/static/js/main.269ea8ee.chunk.js"
+  },
+  {
+    "revision": "218b78c2e4c50f0a1c95692276a8e367",
+    "url": "/static/media/160632.218b78c2.jpg"
   },
   {
     "revision": "8b3a11df8fb599055f0a03d2aaff540d",
     "url": "/static/media/close.8b3a11df.svg"
   },
   {
-    "revision": "d1eff677b582e6914963154edbae6614",
-    "url": "/static/media/vk.d1eff677.svg"
-  },
-  {
     "revision": "e3421fe0b536c77440ca702c8c983d26",
     "url": "/static/media/close1.e3421fe0.svg"
-  },
-  {
-    "revision": "67910a24ef067bf5dc2400ab841889c4",
-    "url": "/static/media/github.67910a24.svg"
   },
   {
     "revision": "ef15ab6acbe9cf150de2022760229795",
     "url": "/static/media/vk1.ef15ab6a.svg"
   },
   {
-    "revision": "db0721b19bf1b047866172744342786f",
-    "url": "/static/media/github1.db0721b1.svg"
+    "revision": "d1eff677b582e6914963154edbae6614",
+    "url": "/static/media/vk.d1eff677.svg"
+  },
+  {
+    "revision": "67910a24ef067bf5dc2400ab841889c4",
+    "url": "/static/media/github.67910a24.svg"
   },
   {
     "revision": "587a4df297afc3d2e3be6be7099de59c",
     "url": "/static/media/twitter.587a4df2.svg"
   },
   {
-    "revision": "c9f1200836515fd4bacd29160be78732",
-    "url": "/static/media/twitter1.c9f12008.svg"
+    "revision": "db0721b19bf1b047866172744342786f",
+    "url": "/static/media/github1.db0721b1.svg"
   },
   {
-    "revision": "b77c17e9e766dc677ffa",
-    "url": "/static/css/1.bbbc7424.chunk.css"
+    "revision": "42ed86b3b3dd970f99f5",
+    "url": "/static/css/1.0106706f.chunk.css"
   },
   {
-    "revision": "b1e0832603ea9081d6fd95f14f8a7ffd",
+    "revision": "8de2f2beac6b4b915a665d952ec7c92b",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d8babcffb5b11e99de24c9bf7a3311f.js"
+  "/precache-manifest.3b469643139a7bb06af61dba907327ea.js"
 );
 
 workbox.clientsClaim();
