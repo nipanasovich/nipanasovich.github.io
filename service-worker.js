a6128f2b46a31c415bac729a7797d7631de5f05a
Nikita Panasovich <nipanasovich@gmail.com>
2019-03-26 20:23:11 +0300

    fixed pop-up

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.516b50c9bbd77cebb73fed348246f15f.js"
+  "/precache-manifest.c5c621f0d7ce546fb79662ceb87ef2fd.js"
 );
 
 workbox.clientsClaim();
